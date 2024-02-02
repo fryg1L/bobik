591bae80765a39ace5440470f3df8d4176b12cda
fryg1L <106621192+fryg1L@users.noreply.github.com>
2024-02-02 12:13:06 +0300

    Add files via upload

---
+++ clouds.py
@@ -0,0 +1,21 @@
+import pygame
+import random
+
+
+class cloud(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.assets = ['sprites/sky3.png', 'sprites/sky2.png', 'sprites/sky4.png']
+        self.enemy = random.choice(self.assets)
+        self.image = pygame.image.load(self.enemy)
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.bottom = random.randint(100, 200)
+        self.rect.x = 1200
+
+    def update(self):
+        self.rect.x -= 10
+        if self.rect.x < -random.randint(1000, 1500):
+            self.enemy = random.choice(self.assets)
+            self.image = pygame.image.load(self.enemy)
+            self.rect.x = 1300
