d0f512af8067d1fad61289374492685b0132f47b
fryg1L <106621192+fryg1L@users.noreply.github.com>
2024-02-05 18:43:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ quit_text = font.render("Покинуть игру", 0, "white")
 main_surface = pygame.image.load('sprites/background.png')
 ground_surface = pygame.image.load('sprites/ground.png')
 
-#start_button = pygame.image.load('sprites/start_button.png')
 count = 0
 text = font.render(f'Cчёт: {count}', 0, 'blue')
 
@@ -44,37 +43,43 @@ running_menu = True
 running_game = False
 running_end_menu = False
 
-
 while activate:
     while running_menu:
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                # этот код реагирует на то, что в области где расположена кнопка старта происходит нажатие
                 if 435 < event.pos[0] < 835 and 200 < event.pos[1] < 300:
                     running_menu = False
                     running_game = True
-                
+
                 elif 435 < event.pos[0] < 835 and 350 < event.pos[1] < 450:
                     running_game = False
                     running_menu = False
                     activate = False
-
 
             if event.type == pygame.QUIT:
                 running_game = False
                 running_menu = False
                 activate = False
 
-        
+        f = open('record.txt', 'r')
+        data = f.read().split('\n')
+        records_text = font.render(f"ТОП 3 РЕКОРДА:", 0, 'blue')
+        record1 = font.render(f"{data[0]}", 0, 'blue')
+        record2 = font.render(f"{data[1]}", 0, 'blue')
+        record3 = font.render(f"{data[2]}", 0, 'blue')
+        f.close()
+
         screen.blit(main_surface, (0, 0))
-        #тут просто кнопка отрисовывается
         screen.blit(button_surface, (435, 200))
         screen.blit(start_text, (480, 215))
         screen.blit(ground_surface, (0, 578))
+        screen.blit(records_text, (1, 0))
+        screen.blit(record1, (9, 50))
+        screen.blit(record2, (9, 100))
+        screen.blit(record3, (9, 150))
 
         screen.blit(quit_but_surface, (435, 350))
         screen.blit(quit_text, (455, 365))
-
 
         decor_sprites.draw(screen)
         decor_sprites.update()
@@ -99,6 +104,22 @@ while activate:
                 activate = False
 
         if pygame.sprite.collide_mask(player, enemies):
+            f = open('record.txt', 'r')
+            date = f.read().split('\n')
+            data = []
+            for x in date:
+                if x != '':
+                    data.append(float(x.strip()))
+            f.close()
+            current_record = round(count, 1)
+            if current_record > data[2]:
+                data.remove(data[2])
+                data.append(current_record)
+                f = open('record.txt', 'w')
+                for x in reversed(sorted(data)):
+                    f.write(str(x) + '\n')
+                f.close()
+
             running_game = False
             running_end_menu = True
 
@@ -122,7 +143,6 @@ while activate:
     while running_end_menu:
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                # этот код реагирует на то, что в области где расположена кнопка старта происходит нажатие
                 if 435 < event.pos[0] < 835 and 200 < event.pos[1] < 300:
                     running_end_menu = False
                     running_game = True
@@ -138,22 +158,31 @@ while activate:
                 activate = False
                 running_end_menu = False
 
-        font = pygame.font.Font('sprites/Minecraft Seven_2.ttf', 40)
         count_text = font.render(f"Ваш результат: {round(count, 1)}", 0, 'blue')
 
+        f = open('record.txt', 'r')
+        data = f.read().split('\n')
+        records_text = font.render(f"ТОП 3 РЕКОРДА:", 0, 'blue')
+        record1 = font.render(f"{data[0]}", 0, 'blue')
+        record2 = font.render(f"{data[1]}", 0, 'blue')
+        record3 = font.render(f"{data[2]}", 0, 'blue')
+        f.close()
 
         screen.blit(main_surface, (0, 0))
-        # тут просто кнопка отрисовывается
+        decor_sprites.draw(screen)
+        decor_sprites.update()
+
         screen.blit(button_surface, (435, 200))
+        screen.blit(records_text, (1, 0))
+        screen.blit(record1, (9, 50))
+        screen.blit(record2, (9, 100))
+        screen.blit(record3, (9, 150))
         screen.blit(start_text, (480, 215))
         screen.blit(ground_surface, (0, 578))
 
         screen.blit(quit_but_surface, (435, 350))
         screen.blit(quit_text, (455, 365))
         screen.blit(count_text, (410, 125))
-
-        decor_sprites.draw(screen)
-        decor_sprites.update()
 
         pygame.display.update()
 
